c99e4a517225645fa220fd5d937d8bb96f10656a
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-21 15:36:09 +0300

    initial commit

---
+++ JustDoIt/View Controllers/Lists View Controller/ListsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListsViewController.swift
+//  JustDoIt
+//
+//  Created by Виктория Бадисова on 21.06.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import UIKit
+
+class ListsViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - View life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    }
+
+}
