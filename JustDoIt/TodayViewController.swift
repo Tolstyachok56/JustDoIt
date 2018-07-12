863983832c0b052ea5371829983c0560b1f2b7ec
Viktoriia Badisova <vbadisova@gmail.com>
2018-07-12 12:15:43 +0300

    add target JUSTDOIT as Today extension

---
+++ JustDoIt/TodayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodayViewController.swift
+//  JUSTDOIT
+//
+//  Created by Виктория Бадисова on 12.07.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
