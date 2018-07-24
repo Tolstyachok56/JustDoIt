0820c4344ba53470b187eb708ca9dd6544f8b925
Viktoriia Badisova <vbadisova@gmail.com>
2018-07-24 14:22:27 +0300

    convert to xCode9 version

--- JustDoIt/Application Delegate/AppDelegate.swift
+++ JustDoIt/Application Delegate/AppDelegate.swift
@@ -14,8 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         
         UNUserNotificationCenter.current().delegate = self
         UIApplication.shared.applicationIconBadgeNumber = 0
